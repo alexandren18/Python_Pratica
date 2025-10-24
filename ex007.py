43f15f3e6bee660f874ab519d5023d5b26306684
alexandren18 <alexandre.nogueira@sempreceub.com>
2025-10-24 19:08:09 -0300

    sétimo commit

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1+n2)/2
+
+print(f"A primeira nota do aluno foi {n1:.2f} e a segunda nota foi {n2:.2f}, a média ficou {m:.2f}")
