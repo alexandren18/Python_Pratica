5d0522f40a9908155561aafeaed160c1ad3ad364
alexandren18 <alexandre.nogueira@sempreceub.com>
2025-10-26 10:07:13 -0300

    décimo terceiro commit

---
+++ ex013.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite seu sálario: "))
+porcentagem = salario * 15 /100
+atual = salario + porcentagem
+
+print(f"Seu antigo sálario era de {salario:.2F}R$ com o aumento de 15% ficou {atual:.2F}R$")
