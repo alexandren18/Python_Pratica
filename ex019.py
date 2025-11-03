2ffa1436c8fbbd5fd71870a0e8df0011abe2177f
alexandren18 <alexandre.nogueira@sempreceub.com>
2025-11-03 00:13:07 -0300

    .

--- ex019.py
+++ ex019.py
@@ -1,8 +1,8 @@
-import random
+from random import choice
 a1 = str(input("Nome do aluno 1: "))
 a2 = str(input("Nome do aluno 2: "))
 a3 = str(input("Nome do aluno 3: "))
 a4 = str(input("Nome do aluno 4: "))
 lista = [a1,a2,a3,a4]
-escolhido = random.choice(lista)
+escolhido = choice(lista)
 print(f"O aluno escolhido foi o {escolhido}")
